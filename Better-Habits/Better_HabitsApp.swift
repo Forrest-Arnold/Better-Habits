d77afa57d564d8a428522bb79ce096a19606083b
Forrest-Arnold <forkalarn@gmail.com>
2025-04-12 17:13:21 -0700

    Initial Commit

---
+++ Better-Habits/Better_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_HabitsApp.swift
+//  Better-Habits
+//
+//  Created by Forrest Kalani Arnold on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Better_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better-HabitsTests/Better_HabitsTests.swift
@@ -0,0 +1,16 @@
+//
+//  Better_HabitsTests.swift
+//  Better-HabitsTests
+//
+//  Created by Forrest Kalani Arnold on 4/12/25.
+//
+
+import Testing
+
+struct Better_HabitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
